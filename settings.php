3e8c20f382ebebe64500cacff12f96ffde437af8
konyakov <konyakov@yandex.ru>
2023-11-18 09:10:29 +0300

    Update

--- settings.php
+++ settings.php
@@ -8,20 +8,20 @@
 $commits = [
 	/* columns
 	'      10>|      20>|      30>|      40>|      50>|  ' < exactly 52 characters */
-	'   *####     #      ###   #####   #   #    *###*    ',  // Sun
-	'   #                  #   #       #  #    #     #   ',  // Mon
-	'   #       #####      #   #       # #     #     #   ',  // Tue
-	'   *###*     #        #   ###     ##*     #     #   ',  // Wed
-	'       #     #        #   #       #  #    #     #   ',  // Thu
-	'       #     #    #   #   #       #   #   #     #   ',  // Fri
-	'   ####*   #####   *##*   #####   #    #   *###*    ',  // Sat
+	'    *#*    ##*     #####  #*   *#   *#*    #    #   ',  // Sun
+	'   #   #   #  #      #     #   #   #   #   #*  *#   ',  // Mon
+	'   #   #   #   #     #      * *    #   #   # ** #   ',  // Tue
+	'   #####   ###*      #       #     #   #   #  # #   ',  // Wed
+	'   #   #   # #       #       #     #   #   #    #   ',  // Thu
+	'   #   #   #  #      #       #     #   #   #    #   ',  // Fri
+	'   #   #   #   #     #       #      *#*    #    #   ',  // Sat
 ];
 //  *  for 10 commmits in that day (lighter color)
 //  #  for 20 commmits in that day (darker color)
 
 
 // Repository origin
-$origin = 'https://github.com/maximal/gerda.git';
+$origin = 'https://github.com/konyakov/gerda.git';
 
 
 // Output shell file
